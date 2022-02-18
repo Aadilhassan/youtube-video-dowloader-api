bdfb108ca30fa318eb852d54f2c1331d41b34a01
Aadilhassan <aahanalhassan@gmail.com>
2022-02-18 20:52:59 +0530

    ind

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-// import {fetch} from "node-fetch";
+
 const youtubedl = require("youtube-dl-exec");
 const express = require("express");
+
 const app = express();
 
-app.get("/favicon.ico", (req, res) => res.send("hello"));
 
 app.get("/", (req, res) => {
   console.log("received request");
   const receivedUrl = req.query.url;
-  const quality = req.query.ql;
-  // Request structure;
-  //http://localhost:8080/?url=https://www.youtube.com/watch?v=6xKWiCMKKJg&ql=1080
-  // youtubedl(receivedUrl, {
-  //   skipDownload: true,
-  //   getUrl: true,
-  //   // format: `[ext!=m3u8][height=240][protocol!=m3u8]`
-  // })
-  //   .then((urla) => {
-  //     res.set("Content-Type", "video/mp4");
-  //     // youtubedl.getInfo(receivedUrl, options, function(err, info) {
-  //     //   if (err) throw err;
-        
-  //       console.log('id:', info.id);
-  //       console.log('title:', info.title);
-  //       console.log('url:', info.url);
-  //       console.log('thumbnail:', info.thumbnail);
-  //       console.log('description:', info.description);
-  //     });
-      
-  //     console.log(urla);
-  //     res.send(`hello ${urla}`);
-      
-  //   })
-  //   .catch((e) => console.log(e.stderr));
-
+  let quality = req.query.ql;
+  let px_480 = "jfvhv";
+  let arr = []
+  if(!quality){
+    quality = 480;
+  } else{
+  }
+  
+  if(quality < 144){
+    quality = 144;
+  } else{
+  }
  
-
-  youtubedl(receivedUrl, {
+youtubedl(receivedUrl, {
     dumpSingleJson: true,
     noWarnings: true,
     noCallHome: true,
@@ -44,9 +29,10 @@ app.get("/", (req, res) => {
     preferFreeFormats: true,
     youtubeSkipDashManifest: true,
     referer: receivedUrl,
-    format: "(mp4,webm)[height<300]",
-  }).then(output =>  res.send(output))
- 
+    format: `(mp4,webm)[height<=${quality}]`,
+  }).then(px => res.send(px)
+  )
+  
 
 }); 
 

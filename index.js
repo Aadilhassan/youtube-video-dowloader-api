5f06d5411666c71c54e13d9a28736651d46e2bae
Aadilhassan <aahanalhassan@gmail.com>
2022-02-18 10:38:13 +0530

    jaso view

--- index.js
+++ index.js
@@ -11,30 +11,44 @@ app.get("/", (req, res) => {
   const quality = req.query.ql;
   // Request structure;
   //http://localhost:8080/?url=https://www.youtube.com/watch?v=6xKWiCMKKJg&ql=1080
-  youtubedl(receivedUrl, {
-    skipDownload: true,
-    getUrl: true,
-    format: `[ext!=m3u8][height=240][protocol!=m3u8]`
-  })
-    .then((urla) => {
-      res.set("Content-Type", "video/mp4");
-      // youtubedl.getInfo(receivedUrl, options, function(err, info) {
-      //   if (err) throw err;
+  // youtubedl(receivedUrl, {
+  //   skipDownload: true,
+  //   getUrl: true,
+  //   // format: `[ext!=m3u8][height=240][protocol!=m3u8]`
+  // })
+  //   .then((urla) => {
+  //     res.set("Content-Type", "video/mp4");
+  //     // youtubedl.getInfo(receivedUrl, options, function(err, info) {
+  //     //   if (err) throw err;
         
-      //   console.log('id:', info.id);
-      //   console.log('title:', info.title);
-      //   console.log('url:', info.url);
-      //   console.log('thumbnail:', info.thumbnail);
-      //   console.log('description:', info.description);
-      // });
+  //       console.log('id:', info.id);
+  //       console.log('title:', info.title);
+  //       console.log('url:', info.url);
+  //       console.log('thumbnail:', info.thumbnail);
+  //       console.log('description:', info.description);
+  //     });
       
-      console.log(urla);
-      res.send(urla);
+  //     console.log(urla);
+  //     res.send(`hello ${urla}`);
       
-    })
-    .catch((e) => console.log(e.stderr));
+  //   })
+  //   .catch((e) => console.log(e.stderr));
 
  
+
+  youtubedl(receivedUrl, {
+    dumpSingleJson: true,
+    noWarnings: true,
+    noCallHome: true,
+    noCheckCertificate: true,
+    preferFreeFormats: true,
+    youtubeSkipDashManifest: true,
+    referer: receivedUrl
+  }).then(output =>  res.send(output))
+ 
+
 });
 
 app.listen("8080");
+
+

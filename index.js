e92baf02b5500131d276537822fb8835971cb36f
Aadilhassan <aahanalhassan@gmail.com>
2022-02-19 17:47:49 +0530

    fyj

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 
 const youtubedl = require("youtube-dl-exec");
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const multer = require('multer');
+var upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(upload.array()); 
+app.use(express.static('public'));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -38,9 +48,44 @@ youtubedl(receivedUrl, {
 
 }); 
 
+app.post("/ma", function (req, res) {
+let  receivedUrl = req.body.basicurl;
+let  quality   = req.body.flexRadioDefault;
 
+if(!receivedUrl){
+  receivedUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+} else{
+}
+if(!quality){
+  quality = 480;
+} else{
+}
+
+if(quality < 144){
+  quality = 144;
+} else{
+}
+
+youtubedl(receivedUrl, {
+  dumpSingleJson: true,
+  noWarnings: true,
+  noCallHome: true,
+  noCheckCertificate: true,
+  preferFreeFormats: true,
+  youtubeSkipDashManifest: true,
+  referer: receivedUrl,
+  format: `(mp4,webm)[height<=${quality}]`,
+}).then(data => res.render("index.ejs", {dat: data})
+)
+
+
+
+
+  // res.render("index.ejs");
+});
 
 app.get("/magic", function (req, res) {
+  
   res.render("index.ejs");
 });
 
